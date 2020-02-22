74e33fd43b6803dcc2792be930e94b33bd0dd5fb
Esteban Flores <ebflores88@gmail.com>
2020-02-22 05:57:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@
 /*Assign an object to the variable playlist and initialize the object
 with a key-value pair — the keys will be artist names and the values
 will be song titles. (What limitation does this impose on our playlist?)*/
-const playlist = { BTS: "Boy with luv"};
+var playlist = { BTS: "Boy with luv"};
 
 /*Create a function updatePlaylist that accepts three parameters:
 the playlist (an object), an artist name (a string), and a song

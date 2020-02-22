c3e6189e6284a30a3fb9ab8ece945efd0db198ce
Esteban Flores <ebflores88@gmail.com>
2020-02-22 05:42:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,4 +12,7 @@ title. The body of the function should add the song and artist as
 a key-value pair to the playlist object. The function should
 return the whole playlist.*/
 
-Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
+/*Create a function removeFromPlaylist that accepts two arguments
+(the playlist object and the artist name). The body of the function
+should delete the key-value pair from the playlist and return the
+updated playlist.*/

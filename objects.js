5072b25ad033db6c551d1e0d59cb912cc6314bb6
Esteban Flores <ebflores88@gmail.com>
2020-02-22 06:35:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -21,6 +21,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 (the playlist object and the artist name). The body of the function
 should delete the key-value pair from the playlist and return the
 updated playlist.*/
+var playlist = { Slowdive: "Alison"};
 function removeFromPlaylist(playlist, artistName){
   return playlist.artistName;
 };

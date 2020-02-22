3c73b5a3b9b3e03a01acef8d76cbd41a8d117a57
Esteban Flores <ebflores88@gmail.com>
2020-02-22 05:59:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,7 +12,7 @@ the playlist (an object), an artist name (a string), and a song
 title. The body of the function should add the song and artist as
 a key-value pair to the playlist object. The function should
 return the whole playlist.*/
-function updatePlaylist(playlist, artist, song){
+function updatePlaylist(playlist, artistName, songTitle){
   //playlist[artist] = song;
   return Object.assign({}, playlist, {[artist]: song});
 };
